675c544209ec96ada1c00c86a7474c2b5d1b4e3b
Joaquin Sotomayor <joaquinsotomayorc@gmail.com>
2017-04-08 01:42:57 -0300

    More configurations to Devise and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  devise_for :users
+  resources :users, only: [:index]
   resources :drying_methods
   resources :product_types
   resources :humidity_samples
   resources :elements
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   get '/' => "elements#index"
 end
