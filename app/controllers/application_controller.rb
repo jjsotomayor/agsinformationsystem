8154ed3e50a99562662012e4ef800bf2648429eb
Joaquin Sotomayor <joaquinsotomayorc@gmail.com>
2017-04-29 01:19:04 -0300

    Configured SeetAlerts, now it can be used

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name])
-    
+    #devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name]) #same but for update
+
   end
 end
