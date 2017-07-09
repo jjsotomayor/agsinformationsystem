0c9dda0df84b61314a14e852645b30112fa07b4f
Joaquin Sotomayor <joaquinsotomayorc@gmail.com>
2017-07-08 22:25:07 -0400

    Changed devise language

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,8 @@ module Agsinformationsystem
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Configurando lenguaje español
+    config.i18n.default_locale = :es
+
   end
 end
