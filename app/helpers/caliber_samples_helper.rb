c01e11ead6bb55251d7886539bf1bf94d7223b94
Joaquin Sotomayor <jjsotomayor@uc.cl>
2018-01-13 18:42:52 -0300

    Default 0's in deviation of tiernizado

--- app/helpers/caliber_samples_helper.rb
+++ app/helpers/caliber_samples_helper.rb
@@ -4,13 +4,21 @@ module CaliberSamplesHelper
     form.number_field :sample_weight, value: 454, readonly: true
   end
 
-  def field_big_fruits_in_sample(type, cal_sample)
-    value = type == "edit" ? cal_sample.deviation_sample.big_fruits_in_sample : ""
+  def field_big_fruits_in_sample(type, cal_sample, default = nil)
+    if default
+      value = default
+    else
+      value = type == "edit" ? cal_sample.deviation_sample.big_fruits_in_sample : ""
+    end
     number_field_tag :big_fruits_in_sample, value,  placeholder: ' cantidad en 283g', min:0
   end
 
-  def field_small_fruits_in_sample(type, cal_sample)
-    value = type == "edit" ? cal_sample.deviation_sample.small_fruits_in_sample : ""
+  def field_small_fruits_in_sample(type, cal_sample, default = nil)
+    if default
+      value = default
+    else
+      value = type == "edit" ? cal_sample.deviation_sample.small_fruits_in_sample : ""
+    end
     number_field_tag :small_fruits_in_sample, value, placeholder: ' cantidad en 283g', min:0
   end
 
