4c975d992e4b7b8c419638d294de0965e46e10dd
Joaquin Sotomayor <jjsotomayor@uc.cl>
2018-01-24 11:31:55 -0300

    Added session expiration

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, key: '_agsinformationsystem_session'
+
+Rails.application.config.session_store :cookie_store, key: '_agsinformationsystem_session', expire_after: 50.minutes
