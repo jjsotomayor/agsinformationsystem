7420ab3c246e28175a4a38d0f6ad63e0f9a76e2f
Joaquin Sotomayor <jjsotomayor@uc.cl>
2018-05-15 01:03:32 -0400

    Fixed, added expiration to devise cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_agsinformationsystem_session'
+# Rails.application.config.session_store :cookie_store, key: '_agsinformationsystem_session'
 
-Rails.application.config.session_store :cookie_store, key: '_agsinformationsystem_session', expire_after: 50.minutes
+Rails.application.config.session_store :cookie_store, key: '_agsinformationsystem_session2', expire_after: 50.minutes
+# NOTE Si se cambia el expiration, tambien cambiar en devise.rb
